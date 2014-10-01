3c3056c6e1fcc8bde366076099eef1409c1f276d
Simon Davies <slndavies@gmail.com>
2014-09-30 22:03:52 -0400

    Initial files added

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// app.js
+
+var app = angular.module("app", ['ngRoute']).config(function($routeProvider) {
+	$routeProvider.when('/login', {
+		templateUrl: 'login.html',
+		controller: 'LoginController'
+	});
+
+	$routeProvider.when('/home', {
+		templateUrl: 'home.html',
+		controller: 'HomeController'
+	});
+
+	$routeProvider.otherwise({
+		redirectTo: '/login'
+	});	
+});
+
+app.controller('LoginController', function() {
+	
+});
+
+app.controller('HomeController', function() {
+	
+});
